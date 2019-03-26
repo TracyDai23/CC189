a578ea16bb9b88a156b1e858c0187046e8758511
TracyDai23 <tracy.dai23@gmail.com>
2019-03-25 23:11:44 -0500

    Update cc_1_1.py

--- cc_1_1.py
+++ cc_1_1.py
@@ -1,9 +1,28 @@
+#Hint asked to find algorithm with Time Complexity as nlog(n). Current solution only have O(n2)
+
+#Dongdong's solution to work only on input string, no additional data structure:
+def isUnique(strs):
+	n=1
+	for item in strs:
+		if item not in strs[:n-1]:
+			n=n+1
+		else: return False
+	return True
+				 
+
+#My list data structure only solution:
 def stringUnique(s):
-	if len(s) == len(set(s)):
-	
+	d = []
+	for i in range(len(s)):
+		if s[i] in d: return False
+		else: d.append(s[i])
+	return True
+
+# My 1st solution:
+def stringUnique(s):
+	if len(s) == len(set(s)):	
 		return True
 	else:
-
 		return False
 
 
